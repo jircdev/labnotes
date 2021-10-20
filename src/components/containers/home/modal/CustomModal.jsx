c01ebada54533243e2d6c99a024e0127c77fd567
Julio Rodriguez <jirc48@gmail.com>
2021-10-20 11:11:31 -0300

    componente

--- src/components/containers/home/modal/CustomModal.jsx
+++ src/components/containers/home/modal/CustomModal.jsx
@@ -2,64 +2,89 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../../../../scss/components/custom-modal.scss";
 import {Children} from "./children";
+export class CustomModal extends React.Component {
 
-export function CustomModal(props) {
+    constructor(props) {
+        super(props);
 
-    const [state, setState] = React.useState({show: props?.show});
-    const modal = React.useRef();
-    const container = document.createElement('div');
+        this.state = {show: (props.show) ? props.show : false};
 
-    const close = async event => {
-        if (event) event.stopPropagation();
-        const {onClose} = props;
-        const body = document.querySelector('body');
-        modal.current.classList.add('modal-hidden');
-        window.setTimeout(async () => {
-            if (typeof onClose === 'function' && !await onClose()) return;
-            setState({show: false, closeClicked: true});
-            body.setAttribute('style', '');
-            body.classList.remove('body-custom-modal-opened');
-        }, 300);
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+        this.onClickContent = this.onClickContent.bind(this);
+        this.onClickBackdrop = this.onClickBackdrop.bind(this);
+
+        this.modal = React.createRef();
+        this._container = document.createElement('div');
+        this._body = document.getElementsByTagName('body')[0];
+    }
+
+    componentDidUpdate() {
+
+        if (this.state.show) {
+            document.querySelector('body').setAttribute('style', 'overflow:hidden');
+        }
+    }
+
+    componentDidMount() {
+
+        if (this.state.show) {
+            document.querySelector('body').setAttribute('style', 'overflow:hidden');
+        }
+
+        this._body.appendChild(this._container);
+    }
+
+    componentWillUnmount() {
+        this._body.removeChild(this._container);
+    }
+
+    async close(event) {
+        const {onClose} = this.props;
+        document.querySelector('body').setAttribute('style', '');
+        if (onClose && typeof onClose === 'function') return onClose(event);
+        this.setState({show: false, closeClicked: true});
+    }
+
+    open() {
+        document.querySelector('body').setAttribute('style', 'overflow:hidden');
+        this.setState({show: true, hideClicked: undefined});
+    }
+
+    onClickBackdrop(event) {
+        if (event.target !== this.modal.current) return;
+        this.close(event);
+    }
+
+    onClickContent(event) {
 
     }
 
-    const onClickBackdrop = event => {
-        event.stopPropagation();
-        if (event.target !== modal.current) return;
-        close(event);
-    }
-    React.useEffect(() => {
-        const body = document.getElementsByTagName('body')[0];
-        body.appendChild(container);
-        return () => body.removeChild(container);
-    }, []);
+    render() {
 
-    const show = state.show && !state.hideClicked;
+        const show = this.state.show && !this.state.hideClicked;
 
-    let cls = 'custom-element-modal '
-    cls += (props.className) ? props.className : '';
+        let cls = 'custom-element-modal '
+        cls += (this.props.className) ? this.props.className : '';
 
-    if (show) cls += ' show-modal';
-    const output = [];
+        if (show) cls += ' show-modal';
+        const output = [];
 
-    if (show) {
-        output.push(
-            <div key="modal-content-wrapper" className="modal-wrapper">
-                <div className="modal-content">
-                    <Children {...props} close={close} key="children-content"/>
+        if (show) {
+            output.push(
+                <div key="modal-content" onClick={this.onClickContent} className="modal-content">
+                    <Children {...this.props} close={this.close} key="children-content"/>
                 </div>
-            </div>
-        );
-    }
+            );
+        }
 
-    return ReactDOM.createPortal(
-        <div
-            ref={modal} onClick={onClickBackdrop}
-            className={cls}>{output}</div>,
-        container
-    );
+        return ReactDOM.createPortal(
+            <div>
+                <div ref={this.modal} onClick={this.onClickBackdrop} className={cls}>{output}</div>
+            </div>,
+            this._container
+        );
+
+    }
 
 }
-
-
-
